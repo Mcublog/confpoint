6fdfdd45d4614a162ced9b3560667722b585b1d3
Viacheslav <slava.k@ks2corp.com>
2022-06-30 14:33:34 +0300

    build: Added script for building Linux exec files

---
+++ py2bin.ps1
@@ -0,0 +1,34 @@
+#!/bin/pwsh
+
+echo "====> Create environment ====>"
+python3 -m venv env
+
+echo "====> Activate it ====>"
+. ./env/bin/activate
+$env:PYTHONPATH="$(pwd)"
+echo "====> Install requirements ====>"
+pip install -r requirements.txt
+
+echo "====> Build exe files ====>"
+
+echo "====> Publisher build ====>"
+pyinstaller --noconfirm --onefile `
+--paths ./venv/Lib/site-packages `
+--console `
+-F "./src/confpoint/publisher/push.py" `
+--distpath "bin" --name "confpoint-publisher"
+
+echo "====> Uploader build ====>"
+pyinstaller --noconfirm --onefile `
+--paths ./venv/Lib/site-packages `
+--console -F "./src/confpoint/uploader/oneloader.py" `
+--distpath "bin" --name "confpoint-uploader"
+
+echo "====> Download build ====>"
+pyinstaller --noconfirm --onefile `
+--paths ./venv/Lib/site-packages `
+--console `
+-F "./src/confpoint/downloader/onedownloader.py" `
+--distpath "bin" --name "confpoint-downloader"
+
+echo "====> Check bin directory"
